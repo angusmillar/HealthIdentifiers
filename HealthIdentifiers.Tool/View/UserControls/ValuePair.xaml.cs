654e3aeb3005c38b43f4db122bebc747e6b11e76
angusmillar <angusbmillar@gmail.com>
2023-05-15 18:52:22 +1000

    Basic structure is coming together for base UI layout

---
+++ HealthIdentifiers.Tool/View/UserControls/ValuePair.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace HealthIdentifiers.Tool.View.UserControls;
+
+public partial class ValuePair : UserControl
+{
+  public ValuePair()
+  {
+    InitializeComponent();
+  }
+}
+
