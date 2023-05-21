64eb9bc3dc58e266f36b01a6436d90021f68ece0
Angus Millar <angusbmillar@gmail.com>
2023-05-22 00:10:06 +1000

    remove public from interface property

--- HealthIdentifiers.Identifiers/Australian/DepartmentVeteransAffairs/IDVANumber.cs
+++ HealthIdentifiers.Identifiers/Australian/DepartmentVeteransAffairs/IDVANumber.cs
@@ -6,8 +6,8 @@
     string Number { get; }
     string SegmentLink { get; }
     string StateCode { get; }
-    string Value { get; }
-    public string ValueDisplay { get; }
+    string Value { get; } 
+    string ValueDisplay { get; }
     string WarCode { get; }
     string CardColor { get; set; }
   }
